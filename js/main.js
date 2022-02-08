c830f0e3d3a8c56a6eaaffa27c6bb0849424457b
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2022-02-08 07:52:59 +0500

    адаптация

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ $(window).scroll(function(){
         $('.header').removeClass('fixed')
     }
 });
+document.querySelector('.header__burgir').addEventListener('click', function() {
+    document.querySelector('.catalog_menu').classList.add('active');
+});
+document.querySelector('.catalog_menu__close').addEventListener('click', function() {
+    document.querySelector('.catalog_menu').classList.remove('active');
+});
+const catalogOpen = document.querySelectorAll('.catalog_menu .open');
+for(let i = 0; i < catalogOpen.length; i++) {
+    catalogOpen[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+};
